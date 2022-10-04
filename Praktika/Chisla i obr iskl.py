935f73d6bd9aaf0406a269af5f52819a4d94ff02
Wydjemko <k7259625@gmail.com>
2022-10-04 16:29:23 +0300

    Initial commit

---
+++ Praktika/Chisla i obr iskl.py
@@ -0,0 +1,9 @@
+import datetime
+kseniya = datetime.date(2002, 11, 26)
+valentina = datetime.date(1998, 2, 27)
+if kseniya > valentina:
+    print('Valentina is older')
+else:
+    print('Kseniya is older')
+
+
